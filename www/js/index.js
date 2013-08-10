d4944987777140147afea471628efcee839ff927
appjopie <appjopie@users.noreply.github.com>
2013-08-10 17:42:38 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,13 @@
 var bodymargin = 5;
 
 function setContainer() {
+			var container = $('#container');
 			$('body').css({padding : bodymargin + 'px' });
 			var containerWidth = $(window).width() - ( bodymargin * 2 );
 			var containerHeight = $(window).height() - ( bodymargin * 2 );
-			$('#container').css({ width : containerWidth + 'px', height : containerHeight + 'px' });
+			container.css({ width : containerWidth + 'px', height : containerHeight + 'px' });
+			container.html(containerWidth + '-' + containerHeight);
+			
 }
 
 $(document).ready(function() {

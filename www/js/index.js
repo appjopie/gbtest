1c18519359f228fdb3644efdedcd777dda42c344
appjopie <appjopie@users.noreply.github.com>
2013-08-10 17:24:45 +0200

    Create index.js

---
+++ www/js/index.js
@@ -0,0 +1,10 @@
+
+var bodymargin = 5;
+
+
+function setContainer() {
+			$('body').css({padding : bodymargin + 'px' });
+			var containerWidth = $(window).width() - ( bodymargin * 2 );
+			var containerHeight = $(window).height() - ( bodymargin * 2 );
+			$('#container').css({ width : containerWidth + 'px', width : containerHeight + 'px' });
+}
